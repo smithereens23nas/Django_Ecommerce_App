e92dc4cce4d4c3fb9651a475217a3df28aa218a6
smiethereens23nas <nicholas23smith@gmail.com>
2022-02-07 23:07:41 -0500

    initial commit message

--- storefront/settings.py
+++ storefront/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+ 
 ROOT_URLCONF = 'storefront.urls'
 
 TEMPLATES = [
