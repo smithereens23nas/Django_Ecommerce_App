d22993e93cd016cbe3760a1935cb876945b4afc1
smiethereens23nas <nicholas23smith@gmail.com>
2022-02-09 13:39:40 -0500

    models are done and makemigrations complete

--- storefront/settings.py
+++ storefront/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "playground", 
+    'store',
+    'tags',
+    'likes'
 ]
 
 MIDDLEWARE = [
